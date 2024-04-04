a684a7becde157ba134d6bbc9e10d8b8d4c472e9
tweeti54 <tweeti54@gmail.com>
2024-04-04 16:10:08 -0400

    update links and more

--- index.php
+++ index.php
@@ -144,11 +144,11 @@
     </footer>
 
     <?php
-    // if (isset($_SESSION['message'])) {
-    //     $message = $_SESSION['message'];
-    //     echo "<script type='text/javascript'>alert('$message');</script>";
-    // }
-    // unset($_SESSION['message']);
+    if (isset($_SESSION['message'])) {
+        $message = $_SESSION['message'];
+        echo "<script type='text/javascript'>alert('$message');</script>";
+    }
+    unset($_SESSION['message']);
     ?>
 </body>
 
